16d02d64948c67d348a68e5c2a2662eb4769ea0d
Chris Gurney <chris@chrisgurney.ca>
2019-04-03 13:04:33 -0400

    Build: Updated for Gulp 4 and minimized dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
-var connect = require('gulp-connect');
-var del = require('del');
-var gulp = require('gulp');
-var includeFile = require('gulp-file-include');
-var inlineSource = require('gulp-inline-source');
-var inlineCss = require('gulp-inline-css');
-var cleanCSS = require('gulp-clean-css');
-/* https://github.com/purifycss/gulp-purifycss */
-var purifyCSS = require('gulp-purifycss');
-var uglify = require('gulp-uglify');
-var markdown = require('markdown');
-var gulpUtil = require('gulp-util');
-var sequence = require('run-sequence');
-var rename = require('gulp-rename');
-var responsive = require('gulp-responsive');
-var rsync = require('gulp-rsync');
-var watch = require('gulp-watch');
+var Connect = require('gulp-connect');
+var Del = require('del');
+var Gulp = require('gulp');
+var Include = require('gulp-file-include');
+var CleanCss = require('gulp-clean-css');
+var Uglify = require('gulp-uglify');
+var Rename = require('gulp-rename');
+var Rsync = require('gulp-rsync');
 
 /* ***** */
 /* Paths */
@@ -50,216 +41,155 @@ var paths = {
   }
 }
 
-/* ******* */
-/* Logging */
-/* ******* */
-
-function logMessage(message) {
-  gulpUtil.log(gulpUtil.colors.blue(message));
-}
-
-function logError(message) {
-  gulpUtil.log(gulpUtil.colors.red(message));
-}
-
 /* ***** */
 /* Tasks */
 /* ***** */
-
-// Purify CSS
-// gulp.task('css', function() {
-//   return gulp.src('./public/app/example.css')
-//     .pipe(purify(['./public/app/**/*.js', './public/**/*.html']))
-//     .pipe(gulp.dest('./dist/'));
-// });
 
 // Minify CSS
 /*
       './css/*.css',
       '!./css/*.min.css'
 */
-gulp.task('minify-css', function() {
-  return gulp.src(paths.src.css)
-    .pipe(cleanCSS())
-    .pipe(rename({
+Gulp.task('minify-css', function(done) {
+
+  return Gulp.src(paths.src.css)
+    .pipe(CleanCss())
+    .pipe(Rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(paths.output.css))
-		.on('finish', function() {
-			logMessage('Completed task: minify-css');
-		});    
+    .pipe(Gulp.dest(paths.output.css));
+
 });
 
-gulp.task('css-inline', function() {
-	return gulp.src(paths.src.html)
-	  .pipe(inlineSource({
-      rootpath: paths.src.base
-    }))
-		.pipe(inlineCss({
-			preserveMediaQueries: true
-		}))
-		.pipe(gulp.dest(paths.output.html))
-		.on('finish', function() {
-			logMessage('Completed task: css-inline');
-		});
-});
-
-// Minify JavaScript
-gulp.task('minify-js', function() {
-  return gulp.src(paths.src.js)
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-		.pipe(gulp.dest(paths.output.js))
-		.on('finish', function() {
-			logMessage('Completed task: minify-js');
-		});
-});
-
-// gulp.task('default', function () {
-//   return gulp.src('src/*.{png,jpg}')
-//     .pipe(responsive({
-//       'background-*.jpg': {
-//         width: 700,
-//         quality: 50
-//       },
-//       'cover.png': {
-//         width: '50%',
-//         // convert to jpeg format
-//         format: 'jpeg',
-//         rename: 'cover.jpg'
-//       },
-//       // produce multiple images from one source
-//       'logo.png': [
-//         {
-//           width: 200
-//         },{
-//           width: 200 * 2,
-//           rename: 'logo@2x.png'
-//         }
-//       ]
+// gulp.task('css-inline', function() {
+// 	return gulp.src(paths.src.html)
+// 	  .pipe(inlineSource({
+//       rootpath: paths.src.base
 //     }))
-//     .pipe(gulp.dest('dist'));
+// 		.pipe(inlineCss({
+// 			preserveMediaQueries: true
+// 		}))
+// 		.pipe(gulp.dest(paths.output.html))
+// 		.on('finish', function() {
+// 			logMessage('Completed task: css-inline');
+// 		});
 // });
 
-gulp.task('include-html', function() {
-  /* return gulp.src([paths.src.html, '!' + paths.src.includes]) */
-  return gulp.src(paths.src.html)
-	  .pipe(includeFile({
+// Minify JavaScript
+Gulp.task('minify-js', function(done) {
+
+  return Gulp.src(paths.src.js)
+    .pipe(Uglify())
+    .pipe(Rename({
+      suffix: '.min'
+    }))
+		.pipe(Gulp.dest(paths.output.js));
+
+});
+
+Gulp.task('include-html', function(done) {
+
+  return Gulp.src(paths.src.html)
+	  .pipe(Include({
 		  prefix: '@@',
 		  basepath: paths.src.includes
 		}))
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: include-html');
-		});		    
+		.pipe(Gulp.dest(paths.output.base));		 
+
 });
 
-gulp.task('copy-css', function() {
-	return gulp.src(paths.src.css)
-		.pipe(gulp.dest(paths.output.css))
-		.on('finish', function() {
-			logMessage('Completed task: copy-css');
-		});
+Gulp.task('copy-css', function(done) {
+
+	return Gulp.src(paths.src.css)
+		.pipe(Gulp.dest(paths.output.css));
+
 });
 
-gulp.task('copy-favicon', function() {
-	return gulp.src(paths.src.favicon)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-favicon');
-		});
+Gulp.task('copy-favicon', function(done) {
+
+	return Gulp.src(paths.src.favicon)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-htaccess', function() {
-	return gulp.src(paths.src.htaccess)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-htaccess');
-		});
+Gulp.task('copy-htaccess', function(done) {
+
+	return Gulp.src(paths.src.htaccess)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-html', function() {
-	return gulp.src(paths.src.html)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-html');
-		});
+Gulp.task('copy-html', function(done) {
+
+	return Gulp.src(paths.src.html)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-images', function() {
-	return gulp.src(paths.src.images)
-		.pipe(gulp.dest(paths.output.images))
-		.on('finish', function() {
-			logMessage('Completed task: copy-images');
-		});
+Gulp.task('copy-images', function(done) {
+
+	return Gulp.src(paths.src.images)
+		.pipe(Gulp.dest(paths.output.images));
+
 });
 
-gulp.task('copy-js', function() {
-	return gulp.src(paths.src.js)
-		.pipe(gulp.dest(paths.output.js))
-		.on('finish', function() {
-			logMessage('Completed task: copy-js');
-		});
+Gulp.task('copy-js', function(done) {
+
+	return Gulp.src(paths.src.js)
+		.pipe(Gulp.dest(paths.output.js));
+
 });
 
-gulp.task('copy-manifest', function() {
-	return gulp.src(paths.src.manifest)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-manifest');
-		});
+Gulp.task('copy-manifest', function(done) {
+
+	return Gulp.src(paths.src.manifest)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-resume', function() {
-	return gulp.src(paths.src.resume)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-resume');
-		});
+Gulp.task('copy-resume', function(done) {
+
+	return Gulp.src(paths.src.resume)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-sw', function() {
-	return gulp.src(paths.src.sw)
-		.pipe(gulp.dest(paths.output.base))
-		.on('finish', function() {
-			logMessage('Completed task: copy-sw');
-		});
+Gulp.task('copy-sw', function(done) {
+
+	return Gulp.src(paths.src.sw)
+		.pipe(Gulp.dest(paths.output.base));
+
 });
 
-gulp.task('copy-vendor', function() {
-	return gulp.src(paths.src.vendor)
-		.pipe(gulp.dest(paths.output.vendor))
-		.on('finish', function() {
-			logMessage('Completed task: copy-vendor');
-		});
+Gulp.task('copy-vendor', function(done) {
+
+	return Gulp.src(paths.src.vendor)
+		.pipe(Gulp.dest(paths.output.vendor));
+
 });
 
-/* * * * * * * * * * * * */
-
-gulp.task('test', function() {
-  /* return gulp.src([paths.src.test, '!' + paths.src.includes + '/**']) */	
-  return gulp.src(paths.src.test)
-	  .pipe(includeFile({
-		  prefix: '@@',
-		  basepath: paths.src.base,
-		  context: {
-		  	mdfile: '\'md/test.md\''
-		  },
-		  filters: {
-        markdown: markdown.parse
-      }
-		}))
-		.pipe(gulp.dest(paths.output.test));	    
-});
+// Gulp.task('test', function() {
+//   /* return gulp.src([paths.src.test, '!' + paths.src.includes + '/**']) */	
+//   return Gulp.src(paths.src.test)
+// 	  .pipe(Include({
+// 		  prefix: '@@',
+// 		  basepath: paths.src.base,
+// 		  context: {
+// 		  	mdfile: '\'md/test.md\''
+// 		  },
+// 		  filters: {
+//         markdown: markdown.parse
+//       }
+// 		}))
+// 		.pipe(Gulp.dest(paths.output.test));	    
+// });
 
 /* *********** */
 /* Clean Tasks */
 /* *********** */
 
-gulp.task('clean', function() {
-  return del([
+Gulp.task('clean', function(done) {
+  return Del([
     paths.output.base
   ]);
 });
@@ -268,8 +198,8 @@ gulp.task('clean', function() {
 /* Combined Tasks */
 /* ************** */
 
-gulp.task('build', function() {
-	sequence(
+Gulp.task('build',
+	Gulp.series(
 		'minify-css',		
 		'copy-favicon',
 		'include-html',
@@ -280,16 +210,20 @@ gulp.task('build', function() {
 		'copy-resume',		
 		'copy-sw',
 		'copy-vendor'			
-	);		
-});
+	),
+	function(done) {
+		done();
+	}
+);	
 
 /* ************ */
 /* Deploy Tasks */
 /* ************ */
 
-gulp.task('deploy', function() {
-  return gulp.src([paths.output.base, '.htaccess'])
-    .pipe(rsync({
+Gulp.task('deploy', function(done) {
+
+  return Gulp.src(paths.output.base, {dot: true})
+    .pipe(Rsync({
       root: paths.output.base,
       recursive: true,
       compress: true,
@@ -299,27 +233,38 @@ gulp.task('deploy', function() {
       hostname: 'chrisgurney.ca',
       destination: 'chrisgurney.ca/'
     }));
+
 });
 
 /* ********** */
 /* Main Tasks */
 /* ********** */
 
-gulp.task('server', function() {
-  connect.server({
+Gulp.task('server', function(done) {
+
+  return Connect.server({
   	root: paths.output.base,
   	livereload: true
   });
+
 });
 
-gulp.task('watch', function () {
-  gulp.watch([paths.src.base + '**/*'], ['build']);
+Gulp.task('watch', function(done) {
+
+ 	return Gulp.watch(
+ 		paths.src.base + '**/*',
+  	Gulp.series('build'));
+
 });
 
-gulp.task('default', function() {
-	sequence(
-		'clean',
-		'build',
-		['server', 'watch']
-	);
-});
+Gulp.task('default',
+	Gulp.series(
+		'clean', 'build',
+		Gulp.parallel(
+			'server',
+			'watch'
+		),
+	function(done) {
+		done();
+	})
+);
