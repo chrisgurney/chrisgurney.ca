d42c0b9bb33abba05632f33d11467612bab06f22
Chris Gurney <chris@chrisgurney.ca>
2019-05-22 09:44:53 -0400

    Magnific Popup support for multiple on one page

--- src/js/creative.js
+++ src/js/creative.js
@@ -93,20 +93,22 @@
   }, 300);
 
   // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: 'title' 
-    }
+  $('.popup-gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: 'title' 
+      }
+    });
   });
 
   $('.popup-youtube').magnificPopup({
